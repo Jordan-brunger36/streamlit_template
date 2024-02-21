d3fe2c5d1d9ea7262cac66fda32478e0836ba76e
Jordan-brunger36 <57197839+Jordan-brunger36@users.noreply.github.com>
2024-02-20 22:50:30 -0700

    modified:   main_page.py

--- main_page.py
+++ main_page.py
@@ -5,5 +5,5 @@ st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer stats")
 
-link = '[Welcome to my github apges site](https://github.com/Jordan-brunger36/Github_assignment)'
+link = '[Welcome to my github apges site](https://github.com/Jordan-brunger36/Github_assignment.git)'
 st.markdown(link, unsafe_allow_html=True)
