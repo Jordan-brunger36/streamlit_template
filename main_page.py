eb16dd47b8712cf063bb7ef187906651ea4a7b5e
Jordan-brunger36 <57197839+Jordan-brunger36@users.noreply.github.com>
2024-02-20 22:47:59 -0700

    adding navigation links

--- main_page.py
+++ main_page.py
@@ -4,3 +4,6 @@ st.markdown("# Welcome to my Website!")
 st.sidebar.markdown("# Main Page")
 
 st.write("Click on a page to see racer stats")
+
+link = '[Welcome to my github apges site](https://github.com/Jordan-brunger36/Github_assignment)'
+st.markdown(link, unsafe_allow_html=True)
